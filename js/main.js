9be63af147a01dcebe38ba10e25aba4f4c3cf050
elvin <elvinyeka@gmail.com>
2022-10-20 23:20:12 +0400

    home page style

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
